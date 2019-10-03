79d4defb67b463c8458cbeba1d12b28ba75dbbef
Yanwen Dong <dongy987@newschool.edu>
2019-10-03 03:10:31 -0400

    add w6 to github page

--- week5/assets/js/main.js
+++ week5/assets/js/main.js
@@ -1,7 +1,23 @@
+// initial ver Sep 27
+// goal: test out the form - interactive website
+
+
 //*** turn into JSON later
 const questions = ["WHERE WOULD YOU SEE ART?", "IS THIS ART?", "EMOJI HERE", "ARE YOU AN ARTIST?", "WHAT KIND OF ART ARE YOU INTERESTED IN?", "DO YOU THINK THIS WEBSITE IS ART?"];
-//*** also: store user replies into JSON
+var caseState = {
+    c0: 0,
+    c1: 1,
+    c2: 2,
+    c3: 3,
+    c4: 4,
+    c5: 5,
+    c6: 6
+}
+var currentCase = caseState.c0;
+
+//*** store user replies into JSON?
 let userAnswer = [];
+
 window.addEventListener('DOMContentLoaded', function () {
     let mobileMenu = $("#mNav");
     mobileLinks(mobileMenu);
@@ -24,12 +40,19 @@ window.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    submitText();
+
+    updateC0();
 });
 
+// for switching case
+function nextIsClicked() {
+    $("#next").click(function () {
+        console.log("haiii");
+        // $('#next').fadeOut(300,function(){$(this).remove();});
+    });
+}
+
 /*
-case0: What do you see as art? [text entry bar with text amount limitation]
-case1: Where would you see art? [multi selector]
 case2: Is this art? (pull random image from API) [y & n button]
 case3: [Invite user to do sth here] [canvas maybe]
 case4: Are you an artist? [slider bar]
@@ -37,7 +60,13 @@ case5: What kind of art are you interested in? [add tags here]
 case6: Do you think this website is art? [slider bar]
 */
 
-function submitText() {
+// case 1: multi selector
+function setupC1(){
+    
+}
+
+// case 0: text entry bar with text amount limitation
+function updateC0() {
     $("#submit").click(function () {
         let response = $('#message').val();
         if (response.length > 0) {
@@ -57,17 +86,7 @@ function submitText() {
     });
 }
 
-function nextIsClicked() {
-    // $("#next").click(function () {
-    //     console.log('clickeeeed');
-    // });
 
-    // to next case
-    $("#next").click(function () {
-        console.log("haiii");
-        $('#next').fadeOut(300,function(){$(this).remove();});
-    });
-}
 
 // for mobile
 // add links
