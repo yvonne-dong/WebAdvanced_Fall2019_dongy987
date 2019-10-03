6dd84d6d4ca3e0ff86bcc350caa3c7b299107ec7
Yanwen Dong <dongy987@newschool.edu>
2019-10-03 17:11:38 -0400

    added button

--- week6/assets/js/main.js
+++ week6/assets/js/main.js
@@ -3,14 +3,14 @@ window.addEventListener('DOMContentLoaded', function () {
     loadData();
 });
 
-function loadData(){
-    $.getJSON("data.json", function(data){
+function loadData() {
+    $.getJSON("data.json", function (data) {
         console.log(data);
         generateWebsites(data);
     });
 }
 
-function generateWebsites(data){
+function generateWebsites(data) {
     let source = $("#hbar-template").html();
     let template = Handlebars.compile(source);
     let result = template(data);
@@ -18,4 +18,13 @@ function generateWebsites(data){
     let list = $(".artworks-list");
 
     list.append(result);
+}
+
+function mouseOver() {
+    // var color = '#' + Math.floor(Math.random() * 16777215).toString(16);
+    // document.getElementById("buttons").style['background-color'] = color;
+}
+
+function mouseOut() {
+
 }
